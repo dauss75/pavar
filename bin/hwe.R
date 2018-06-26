933685092ecea12a1ac53f5b4296e5b9190ed1f5
dauss75 <dauss75@gmail.com>
2018-06-26 10:48:47 -0500

    parsing update

--- bin/hwe.R
+++ bin/hwe.R
@@ -1,6 +1,6 @@
 packages <- c("data.table", "HardyWeinberg", "optparse")
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-   install.packages(setdiff(packages, rownames(installed.packages())),repos = "http://cran.us.r-project.org")
+  install.packages(setdiff(packages, rownames(installed.packages())),repos = "http://cran.us.r-project.org")
 }
 suppressPackageStartupMessages(library(HardyWeinberg))
 suppressPackageStartupMessages(library(optparse))
@@ -15,44 +15,104 @@ opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(OptionParser(option_list=option_list))
 
 if ( is.null(opt$v) & is.null(opt$i)) {
-    print_help(opt_parser)
-    stop("Missing input files path!\n")
+  print_help(opt_parser)
+  stop("Missing input files path!\n")
 } else {
-    gnomad<-read.table(opt$v, fill=TRUE, sep="\t")
-    paver<-read.table(opt$i, fill=TRUE, sep="\t")
+  gnomad<-fread(opt$v); paver<-fread(opt$i)
+  if(nrow(paver)==0){
+    quit()
+  }
 }
 
-combo<-merge(gnomad,paver,by="V2")
-HWE.test<-as.data.frame(matrix(, nrow = nrow(combo), ncol = 15))
+## temporary variables
+gnomad<-fread("/Users/sjung/Documents/GitHub/pavar/output/gnomad/wes/chr3/input_vcf/IQCB1.vcf");
+paver<-fread("/Users/sjung/Documents/GitHub/pavar/output/gnomad/wes/chr3/IQCB1.pathogenic.intervar.txt")
+
+##
+
+colnames(gnomad)[1:5]<-c("Chr","Start","dbsnp","Ref","Alt")
+combo<-merge(gnomad,paver,by="Start")
+
+# HWE.test<-as.data.frame(matrix(, , ncol = 15))
+HWE.test<-as.data.frame(matrix(, , ncol = 16))
 us.pop=326766748
-colnames(HWE.test)<-c("Chr","Pos","GC_MM","GC_MN","GC_NN","p-val","HWE","AF","AF_male","AF_female","LCA_AF","LCA_AF_male","LCA_AF_female","q=Sum(AF)","Carrier")
+colnames(HWE.test)<-c("Chr","Pos","GC_MM","GC_MN","GC_NN","p-val","HWE","AF","AF_male","AF_female","LCA_AF","LCA_AF_male","LCA_AF_female","GC_population", "q=Sum(AF)","Estimated LCA")
 alpha=0.05
 for (i in 1:nrow(combo)){
-  string=combo$V8.x[i]
-  t<-as.numeric(unlist(strsplit(gsub('.*GC=(.*);AF_raw=.*','\\1',string),",")))
-  AF<-as.numeric(unlist(strsplit(gsub('.*;AF=(.*);AN=.*','\\1',string),",")))
-  lca.AF<-floor(AF[1]*us.pop)
-  AF_male<-as.numeric(unlist(strsplit(gsub('.*AF_Male=(.*);AF_Female=.*','\\1',string),",")))
-  lca.AF_male<-floor(AF_male[1]*(us.pop/2))
-  AF_female<-as.numeric(unlist(strsplit(gsub('.*;AF_Female=(.*);GC_Male=.*','\\1',string),",")))
-  lca.AF_female<-floor(AF_female[1]*(us.pop/2))
-
-  HWE.test[i,1:2]<-c(combo$V1.y[i],combo$V3.y[i])
-  HWE.test[i,3:5]<-c(MM=t[1],MN=t[2],NN=t[3]); x<-c(MM=t[1],MN=t[2],NN=t[3])
-  # chi-square test without Yates’ continuity correction, which is not recommended for low minor allele frequencies
-  HWE.test[i,6] <- HWChisq(x, cc = 0, verbose = FALSE)$pval
-  if (HWE.test[i,6] < alpha){
-    HWE.test[i,7]<-"FALSE"
+  if (length(unlist(strsplit(combo$Alt.x[i],","))) > 1){
+    # find position of the allele
+    string=combo$V8[i]
+    index=which(unlist(strsplit(combo$Alt.x[i],",")) == combo$Alt.y[i])
+    
+    GC<-unlist(strsplit(gsub('.*;GC=(.*);.*','\\1',string),";"))[1];
+    GC_content=as.numeric(unlist(strsplit(GC,","))[(3*(index-1)+1):(3*(index-1)+3)])
+    AF<-as.numeric(unlist(strsplit(gsub('.*;AF=(.*);AN=.*','\\1',string),",")))[index]
+    # print(AF)
+    lca.AF<-floor(AF[1]*us.pop)
+    AF_male<-as.numeric(unlist(strsplit(gsub('.*AF_Male=(.*);AF_Female=.*','\\1',string),",")))[index]
+    lca.AF_male<-floor(AF_male[1]*(us.pop/2))
+    AF_female<-as.numeric(unlist(strsplit(gsub('.*;AF_Female=(.*);GC_Male=.*','\\1',string),",")))[index]
+    lca.AF_female<-floor(AF_female[1]*(us.pop/2))
+    
+    HWE.test[i,1:2]<-c(combo$Chr[i],combo$Start[i])
+    HWE.test[i,3:5]<-c(MM=GC_content[1],MN=GC_content[2],NN=GC_content[3]); x<-c(MM=GC_content[1],MN=GC_content[2],NN=GC_content[3])
+    # print(x)
+    
+    # chi-square test without Yates’ continuity correction, which is not recommended for low minor allele frequencies
+    if (sum(x) > 0 & x[1]>5000){  # temporary value
+      HWE.test[i,6] <- HWChisq(x, cc = 0, verbose = FALSE)$pval
+      if (HWE.test[i,6] < alpha){
+        HWE.test[i,7]<-"FALSE"
+      } else {
+        HWE.test[i,7]<-"TRUE"
+      }
+      HWE.test[i,8:10]<-c(AF[1],AF_male[1],AF_female[1])
+      HWE.test[i,11:13]<-c(lca.AF,lca.AF_male,lca.AF_female)
+      HWE.test[i,14]<-GC_population<-gsub('.*;(GC_AFR=.*);GC_Male=.*','\\1',string)
+    } else {
+      HWE.test<-HWE.test[-i,]
+      next
+    }
+    
   } else {
-    HWE.test[i,7]<-"TRUE"
+    # find position of the allele
+    string=combo$V8[i]
+    GC<-unlist(strsplit(gsub('.*;GC=(.*);.*','\\1',string),";"))[1];
+    GC_content=as.numeric(unlist(strsplit(GC,",")))
+    AF<-as.numeric(unlist(strsplit(gsub('.*;AF=(.*);AN=.*','\\1',string),";")))
+    # print(AF)
+    lca.AF<-floor(AF[1]*us.pop)
+    AF_male<-as.numeric(unlist(strsplit(gsub('.*AF_Male=(.*);AF_Female=.*','\\1',string),";"))[1])
+    lca.AF_male<-floor(AF_male[1]*(us.pop/2))
+    AF_female<-as.numeric(unlist(strsplit(gsub('.*;AF_Female=(.*);GC_Male=.*','\\1',string),";"))[1])
+    GC_population<-gsub('.*;(GC_AFR=.*);GC_Male=.*','\\1',string)
+    lca.AF_female<-floor(AF_female[1]*(us.pop/2))
+    
+    HWE.test[i,1:2]<-c(combo$Chr[i],combo$Start[i])
+    HWE.test[i,3:5]<-c(MM=GC_content[1],MN=GC_content[2],NN=GC_content[3]); x<-c(MM=GC_content[1],MN=GC_content[2],NN=GC_content[3])
+    
+    # chi-square test without Yates’ continuity correction, which is not recommended for low minor allele frequencies
+    if (sum(x) > 0 & x[1]>1000){
+      HWE.test[i,6] <- HWChisq(x, cc = 0, verbose = FALSE)$pval
+      if (HWE.test[i,6] < alpha){
+        HWE.test[i,7]<-"FALSE"
+      } else {
+        HWE.test[i,7]<-"TRUE"
+      }
+      HWE.test[i,8:10]<-c(AF[1],AF_male[1],AF_female[1])
+      HWE.test[i,11:13]<-c(lca.AF,lca.AF_male,lca.AF_female)
+      HWE.test[i,14]<-GC_population<-gsub('.*;(GC_AFR=.*);GC_Male=.*','\\1',string)
+    } else {
+      HWE.test<-HWE.test[-i,]
+      next
+    }
+    
   }
-  HWE.test[i,8:10]<-c(AF[1],AF_male[1],AF_female[1])
-  HWE.test[i,11:13]<-c(lca.AF,lca.AF_male,lca.AF_female)
 }
-
-sig_q <- sum(HWE.test[,8])
-carrier <- floor(sig_q*sig_q*us.pop)
-HWE.test[1,14:15]<-c(sig_q,carrier)
-
-#write.table(HWE.test, opt$o, sep="\t", quote=FALSE, row.names=FALSE)
+#print(na.omit(HWE.test[,8]))
+sig_q <- sum(na.omit(HWE.test[,8]))
+# print(sig_q)
+LCA <- floor(sig_q*sig_q*us.pop)
+# print(carrier)
+HWE.test[1,15:16]<-c(sig_q,LCA)
 fwrite(HWE.test, opt$o, sep="\t")
